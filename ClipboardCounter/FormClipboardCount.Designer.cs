54e67a6120815ac7b1bb61255075c28c7bbb6e2a
nan0-a <b185334@hiroshima-u.ac.jp>
2023-06-08 23:18:08 +0900

    first commit

---
+++ ClipboardCounter/FormClipboardCount.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace ClipboardCounter
+{
+    partial class FormClipBoardCount
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.char_count_label = new System.Windows.Forms.Label();
+            this.clipboard_check_fire = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // char_count_label
+            // 
+            this.char_count_label.AutoSize = true;
+            this.char_count_label.Location = new System.Drawing.Point(12, 9);
+            this.char_count_label.Name = "char_count_label";
+            this.char_count_label.Size = new System.Drawing.Size(62, 15);
+            this.char_count_label.TabIndex = 0;
+            this.char_count_label.Text = "Not A Text";
+            // 
+            // clipboard_check_fire
+            // 
+            this.clipboard_check_fire.Enabled = true;
+            this.clipboard_check_fire.Tick += new System.EventHandler(this.ClipboardCheckFireTick);
+            // 
+            // FormClipBoardCount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(160, 32);
+            this.Controls.Add(this.char_count_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FormClipBoardCount";
+            this.Text = "ClipBoardCount";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormClipboardCounterLoad);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label char_count_label;
+        private System.Windows.Forms.Timer clipboard_check_fire;
+    }
+}
+
---
+++ ClipboardCounter/FormClipboardCount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipboardCounter
+{
+    public partial class FormClipBoardCount : Form
+    {
+        public FormClipBoardCount()
+        {
+            InitializeComponent();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        private void FormClipboardCounterLoad(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            ClipboardCheckFireTick(null, null);
+        }
+
+        private void ClipboardCheckFireTick(object sender, EventArgs e)
+        {
+            string cboard_cont, cboard_cont_noline;
+            int cboard_cont_byte;
+            IDataObject data = Clipboard.GetDataObject();
+
+            if (data.GetDataPresent(DataFormats.Text))
+            {
+                cboard_cont = (string)data.GetData(DataFormats.Text);
+
+                //CRLFをLFに置換
+                cboard_cont = cboard_cont.Replace("\r\n", "\n");
+                cboard_cont_noline = cboard_cont.Replace("\n", "");
+
+                //文字コードをShift-JISとしてバイト数を取得する
+                Encoding encShiftJis = Encoding.GetEncoding("Shift_JIS");
+                cboard_cont_byte = encShiftJis.GetByteCount(cboard_cont);
+
+                int threads = (int)Math.Ceiling((double)cboard_cont_byte / 280),
+                    bytes_left = cboard_cont_byte % 280 > 0 ? 280 - cboard_cont_byte % 280 : 0;
+
+                // char_count_label.Text = "Chars: " + cboard_cont.Length.ToString() 
+                // + "\nChars(w/o \\n): " + cboard_cont_noline.Length.ToString()
+                // + "\nTwitter: " + threads.ToString()
+                // + "(-" + bytes_left.ToString() + "B)";
+
+                char_count_label.Text = cboard_cont.Length.ToString()
+                    + " : " + cboard_cont_noline.Length.ToString()
+                    + " : " + threads.ToString()
+                    + " : -" + bytes_left.ToString() + "B";
+            }
+            else
+            {
+                char_count_label.Text = "Not A Text";
+            }
+        }
+    }
+}
